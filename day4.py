447651da2840a3d7a6d9ef9f0e3c5fff11e1fa51
Jason Wang <jwang@captechventures.com>
2022-12-04 04:23:15 -0500

    day 4 clarification

--- day4.py
+++ day4.py
@@ -7,7 +7,7 @@ with open('inputs/day4data.txt', 'r') as file:
         range2a, range2b = range2.split('-')
         range1 = range(int(range1a), int(range1b) + 1)
         range2 = range(int(range2a), int(range2b) + 1)
-        # check if one fully overlaps another
+        # check if one fully overlaps another by checking if the first and last values of one range fall into the other range
         if (range1[0] in range2 and range1[-1] in range2) or (range2[0] in range1 and range2[-1] in range1):
             counter += 1
     print(counter)
